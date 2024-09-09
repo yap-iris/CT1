c2ee87a31b25cca85284773f6f225a937a16ba1d
yap-iris <pkyap@iris.com.my>
2024-09-09 15:39:42 +0800

    Add files via upload

---
+++ python-job-dsl.groovy
@@ -0,0 +1,24 @@
+job('Python-Build-Job') {
+    description('A Jenkins job to build Python projects.')
+
+    // Source Code Management
+    scm {
+        git('https://github.com/yap-iris/CT1.git')
+    }
+
+    // Build Triggers
+    triggers {
+        scm('H/15 * * * *')  // Poll SCM every 15 minutes
+    }
+
+    // Build Steps
+    steps {
+        shell('pip install -r requirements.txt')  // Install dependencies
+        shell('pytest')  // Run tests
+    }
+
+    // Post-build Actions
+    publishers {
+        archiveArtifacts('**/target/*.zip')
+    }
+}
